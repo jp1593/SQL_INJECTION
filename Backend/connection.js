c6d6618e8c5bd553574403e1bf647c3d9947e72b
jp1593 <juanpablo08082002@hotmail.com>
2024-08-15 14:34:59 -0600

    Init nodejs backend and connection to database

---
+++ Backend/connection.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const mysql = require("mysql2/promise");
+
+const config = {
+  host: "mysql-2f7acbf4-sqlinjection-3486.h.aivencloud.com",
+  user: "avnadmin",
+  password: process.env.PASS,
+  database: "defaultdb",
+  port: "18312",
+};
+
+async function connectToDatabase() {
+  try {
+    const connection = await mysql.createConnection(config);
+    console.log("Connected to Aiven MySQL database");
+
+    await connection.end();
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
+}
+
+connectToDatabase();
